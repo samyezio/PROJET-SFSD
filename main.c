a6489d7adb951ec3c59b699bffcce6ab514edeaf
samyezio <144060412+samyezio@users.noreply.github.com>
2024-01-19 23:06:22 +0100

    les difinition de structures.
    la structure genral d'etudiant ,avec les information dans lentete du fichier et l'entete du block.

--- main.c
+++ main.c
@@ -1,10 +1,38 @@
 #include <stdio.h>
-#include<stdlib.h>
+#include <stdlib.h>
+#include <string.h>
+typedef struct Etudiant{
+    char matricule[20];
+    char nom[100];
+    char prenom[100];
+    float moyenne;
+}Etudiant;
+ typedef struct entete_fichier{
+   size_t file_size;
+   char file_name[100];
+   char file_extension[30];
+   int nb_element;
+ }file_header;
+typedef struct block_entete{
+    size_t block_size;
+    int nb_block_element;
+    int facteur_blockage;
+    char data_type[50];
+}block_entete;
+
+
+
+
+
+
+
+
+
+
+
 int main(){
 
-   printf("hello world");
-   printf("Anis alloun ");
-   printf("suuuuu");
+  
    
 
 
