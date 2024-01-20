4e7e37e0e7e3cc1fd97785a76c16a43c90ae6ba8
SidAhmedHim <155841137+SidAhmedHim@users.noreply.github.com>
2024-01-20 02:49:19 +0100

    fonction search V1.0
    Rechargeons un élément en utilisant son matricule. Mettons les éléments d'un bloc dans un tableau, puis parcourons-le pour rechercher la valeur souhaitée

--- main.c
+++ main.c
@@ -12,13 +12,64 @@ typedef struct Etudiant{
    char file_name[100];
    char file_extension[30];
    int nb_element;
- }file_header;
+ }entete_fichier;
 typedef struct block_entete{
     size_t block_size;
     int nb_block_element;
     int facteur_blockage;
     char data_type[50];
 }block_entete;
+
+
+
+void read_element(Etudiant x){
+
+    printf("matricule) : %s \n",x.matricule);
+    printf("nom / prenom : %s / %s \n",x.nom,x.prenom);
+    printf("moyenne : %s \n",x.moyenne);
+}
+
+void search (char *file_path, char *mat){
+    FILE *fic=fopen(*file_path,"rb");
+    entete_fichier Ftete;
+    fread(&Ftete,sizeof(entete_fichier),1,fic);
+    Etudiant T[Ftete.nb_element]; 
+    block_entete Tbloc;
+    int trouver =0,  i=0 ,c ,j;
+    while(fread(&Tbloc,sizeof(block_entete),1,fic)> 0 && trouver == 0){
+        if(Tbloc.nb_block_element!=0){
+            fread(T+i,sizeof(Etudiant),Tbloc.nb_block_element,fic){
+                c=Tbloc.nb_block_element;
+                j=i;
+                while(c>0 && trouver==0){
+                    if(strcmp(T[j].matricule,mat)==0){
+                        trouver=1;
+                        read_element(t[j]);
+                    }j++;  c--;
+                    }
+               i=i+Tbloc.nb_block_element;     
+
+
+
+            }
+
+
+
+
+        }
+
+
+
+
+    }
+   
+
+
+
+
+
+}
+
 
 
 
