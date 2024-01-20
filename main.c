7f4e4eb07cb31fd64e5cf99a4ccda5a3143e9ef4
ALLOUNanis <155636633+ALLOUNanis@users.noreply.github.com>
2024-01-20 03:02:52 +0100

    finale1

--- main.c
+++ main.c
@@ -8,7 +8,6 @@ typedef struct Etudiant{
     char prenom[100];
     float moyenne;
 }Etudiant;
-
  typedef struct entete_fichier{
    size_t file_size;
    char file_name[100];
@@ -22,21 +21,98 @@ typedef struct block_entete{
     char data_type[50];
 }block_entete;
 
+void insertion_block (char *file_path, Etudiant *T ,int facteur_blockage,n){
+    FILE *file= fopen(file_path,"r+b");
+    fseek(file,0,SEEK_END);
+    block_entete block;
+    entete_fichier entete;
+    strcpy(block.data_type,"Etudiant");
+    block.facteur_blockage=facteur_blockage;
+    void *buffer;
+    for (int i =0 ;i < n ; i=i+facteur_blockage){
+        int facteur;
+        if (i + facteur_blockage > n ){
+            facteur= facteur_blockage
+             while (i + facteur > n ) 
+        {
+           facteur--;    
+        } 
+        block.nb_block_element= facteur;
+        }
+        else{
+            facteur=facteur_blockage;
+            block.nb_block_element=facteur_blockage;
+
+        }
+        block.block_size = sizeof(Etudiant) * facteur_blockage;
+        size_t block_memory_size = sizeof(block_header) + block.block_size;
+        buffer = malloc(block_memory_size);
+        memcpy( buffer , &block , sizeof(block_header) );
+        memcpy (buffer + sizeof (block_header),(Etudiant *)T+i,sizeof (Etudiant)*facteur);
+        fwrite (buffer,block_memory_size,1,file);
+        free(buffer);
+        
+    }
+
+}
+
+
+
+void read_element(Etudiant x){
+
+    printf("matricule) : %s \n",x.matricule);
+    printf("nom / prenom : %s / %s \n",x.nom,x.prenom);
+    printf("moyenne : %s \n",x.moyenne);
+}
 
 
 
 
 
+void search (char *file_path, char *mat){
+    FILE *fic=fopen(*file_path,"rb");
+    entete_fichier Ftete;
+    fread(&Ftete,sizeof(entete_fichier),1,fic);
+    Etudiant T[Ftete.nb_element]; 
+    block_entete Tbloc;
+    int trouver =0,  i=0 ,c ,j;
+    while(fread(&Tbloc,sizeof(block_entete),1,fic)> 0 && trouver == 0){
+        if(Tbloc.nb_block_element!=0){
+            fread(T+i,sizeof(Etudiant),Tbloc.nb_block_element,fic){
+                c=Tbloc.nb_block_element;
+                j=i;
+                while(c>0 && trouver==0){
+                    if(strcmp(T[j].matricule,mat)==0){
+                        trouver=1;
+                        read_element(t[j]);
+                    }j++;  c--;
+                    }
+               i=i+Tbloc.nb_block_element;     
+
+
+
+            }
 
 
 
 
+        }
 
-int main(){
 
-  
+
+
+    }
    
 
 
-    return 0;
+
+
+
+}
+
+
+int main ()
+{
+
+    return 0 ;
 }
